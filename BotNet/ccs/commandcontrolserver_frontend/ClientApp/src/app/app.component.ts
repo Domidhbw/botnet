a65f6b52a19044596dd079f67bf2bc8a2536441a
Domidhbw <dom.neubauer.23@lehre.mosbach.dhbw.de>
2025-02-25 16:38:36 +0100

    revert

--- BotNet/ccs/commandcontrolserver_frontend/ClientApp/src/app/app.component.ts
+++ BotNet/ccs/commandcontrolserver_frontend/ClientApp/src/app/app.component.ts
@@ -1,32 +1,9 @@
 import { Component } from '@angular/core';
-import { Bot } from './components/bot-list/bot-list.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  showBotEditModal = false;
-  showGroupsModal = false;
-  showFileModal = false;
-
-  onBotSelectionChanged(bots: Bot[]): void {
-    console.log('Aktuelle Bot-Auswahl:', bots);
-  }
-
-  openBotEditModal(bot: Bot): void {
-    console.log('Öffne Bot-Bearbeitungsmodal für:', bot);
-    this.showBotEditModal = true;
-  }
-
-  openGroupsModal(): void {
-    console.log('Öffne Gruppenmodal');
-    this.showGroupsModal = true;
-  }
-
-  openFileModal(bot: Bot): void {
-    console.log('Öffne Filemodal für:', bot);
-    this.showFileModal = true;
-  }
+  title = 'app';
 }
