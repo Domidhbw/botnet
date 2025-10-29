26795f6439b315e6ac0509d9a7c52d5da0ad421b
Neubauer, Dominik <dominik.neubauer@wuerth-industrie.com>
2025-10-29 21:44:53 +0100

    Update CommandController.cs

--- BotNet/bot/Bot.Api/Controllers/CommandController.cs
+++ BotNet/bot/Bot.Api/Controllers/CommandController.cs
@@ -23,13 +23,6 @@ public async Task<IActionResult> RunCommand([FromQuery] string cmd)
         return BadRequest(new { error = "Command cannot be empty" });
     }
 
-    // Local constants: remove access modifiers
-    const string DemoPassword = "SuperSecret123!";
-    const string ConnString   = "Server=localhost;Database=demo;User Id=sa;Password=MyS3cret!;";
-    const string GitHubToken  = "ghp_0123456789ABCdef0123456789ABCdef0123";
-    const string AwsAccessKeyId     = "AKIA1234567890ABCD12";
-    const string AwsSecretAccessKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
-
     var result = await _commandService.ExecuteCommandAsync(cmd);
     return Ok(result);
 }
